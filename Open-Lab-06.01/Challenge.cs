24f55a877a9149ba3aaf5907a60a89a164edf56f
DarkVoidSvk <samueljakubecsk@gmail.com>
2021-10-29 21:23:34 +0200

    Implemented the method MysteryFunc(string str) that will satisfy the relation between the input and the output.

--- Open-Lab-06.01/Challenge.cs
+++ Open-Lab-06.01/Challenge.cs
@@ -5,8 +5,22 @@ namespace Open_Lab_06._01
     public class Challenge
     {
         public string MysteryFunc(string str)
-        {
-            throw new NotImplementedException();
+        { 
+            string new_string = String.Empty;
+            char[] characters = str.ToCharArray();
+
+            for (int i = 0; i <= characters.Length - 1; i += 1)
+            {
+                if (int.TryParse(characters[i].ToString(), out int not_needed) == false)
+                {
+                    bool successfullyParsed = int.TryParse(characters[i + 1].ToString(), out int Length_Of_Letter);
+                    for (int a = 1; a <= Length_Of_Letter; a++)
+                    {
+                        new_string += characters[i].ToString();
+                    }
+                }
+            }
+            return new_string;
         }
     }
 }
